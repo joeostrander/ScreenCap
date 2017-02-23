0a7ca728c40f3fabcce99785cf8a6a5046a0c42f
joeostrander <ostrander.jm@gmail.com>
2017-02-23 16:03:05 -0600

    added choice of silent of not

--- ScreenCap/Program.cs
+++ ScreenCap/Program.cs
@@ -4,29 +4,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
-
+using System.Runtime.InteropServices;
 
 namespace ScreenShot
 {
     class Program
     {
 
+        //[DllImport("kernel32.dll")]
+        //static extern bool AttachConsole(UInt32 dwProcessId);
+        //private const UInt32 ATTACH_PARENT_PROCESS = 0xFFFFFFFF;
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll")]
+        private static extern bool AllocConsole();
+
+        static String strFilePath = "desktop.jpg";
+        static String output = "";
+        static bool boolShowUsage = false;
+        static bool boolSilent = false;
 
         static void Main(string[] args)
         {
             if (args.Length > 0)
             {
-                Console.WriteLine();
-                Console.WriteLine("Joe Ostrander");
-                Console.WriteLine("Use this to copy the desktop to a file from command line.");
-                Console.WriteLine("2017.02.23");
-                Console.WriteLine();
+
+                if (args.Contains("/?")) {
+                    boolShowUsage = true;
+                }
+                else
+                {
+                    if (System.IO.Directory.Exists(args[0]))
+                    {
+                        strFilePath = args[0] + "\\" + strFilePath;
+                    }
+                    else
+                    {
+                        output += "Directory not found:  " + args[0] + "\n\n";
+                        boolShowUsage = true;
+                    }
+                }
+
+                if (args.Contains("/silent"))
+                {
+                    boolSilent = true;
+                }
+
+            }
+
+            if (boolShowUsage==false)
+            {
+                TakeScreenshot();
+            }
+
+            if (boolSilent)
+                Environment.Exit(0);
+
+
+            AllocConsole();
+            //AttachConsole(ATTACH_PARENT_PROCESS);
+
+            if (boolShowUsage)
+            {
+                output+="Joe Ostrander\n";
+                output += "2017.02.23\n";
+                output += "Take a screenshot and save to directory\n\n";
+                output += "Usage:  \n"; ;
+                output += System.Windows.Forms.Application.ProductName + "\n";
+                output += "or\n";
+                output += System.Windows.Forms.Application.ProductName + " <directory name>\n";
+                output += "or\n";
+                output += System.Windows.Forms.Application.ProductName + " <directory name> /silent\n\n";
+                output += "Press ENTER to continue...";
+
+                if (boolSilent == false)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(output);
+                    Console.ReadLine();
+                }
 
             }
             else
             {
-                TakeScreenshot();
+                if (boolSilent == false)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(output);
+                    Console.WriteLine();
+                    System.Threading.Thread.Sleep(2000);
+                }
+
             }
+            
             
         }
 
@@ -37,12 +106,13 @@ namespace ScreenShot
             try
             {
                 Image bmTemp = SC.CaptureScreen();
-                bmTemp.Save("desktop.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
-                Console.WriteLine("Desktop captured to:  desktop.jpg");
+                bmTemp.Save(strFilePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                output += "Desktop captured to:  " + strFilePath + "\n";
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                output += ex.Message + "\n";
             }
 
 
